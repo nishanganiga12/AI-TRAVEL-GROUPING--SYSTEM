6813aa86666edb96928b4e29057e97b31d84a982
nishanganiga12 <12nishan2004@gmail.com>
2026-03-20 22:39:01 +0530

    decreased password

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ def login():
     password = st.text_input("🔑 Password", type="password")
 
     if st.button("Login"):
-        if username == "admin" and password == "1234":
+        if username == "admin" and password == "123":
             st.session_state["logged_in"] = True
         else:
             st.error("❌ Invalid credentials")
